c0421b7cdc920e8991a9eaf6b1daff6f5964b14e
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-11 22:01:26 -0300

    .

--- backend/humanresources-api/src/main/java/br/com/humanresources/HumanresourcesApiApplication.java
+++ backend/humanresources-api/src/main/java/br/com/humanresources/HumanresourcesApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HumanresourcesApiApplication {
 
+	// Teste
 	public static void main(String[] args) {
 		SpringApplication.run(HumanresourcesApiApplication.class, args);
 	}
