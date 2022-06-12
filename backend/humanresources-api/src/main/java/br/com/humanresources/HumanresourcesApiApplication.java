c2632200b7782a1dd354a341dbc40d14b048fa9e
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-06-11 21:57:25 -0300

    Project created

---
+++ backend/humanresources-api/src/main/java/br/com/humanresources/HumanresourcesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.humanresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesApiApplication.class, args);
+	}
+
+}
